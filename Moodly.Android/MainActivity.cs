e61b6395e4117fc6c3672e6d103dfb04dbf42550
Wiktoria Zelawska <wikzel13@gmail.com>
2026-01-05 02:40:48 +0100

    zmiana nazwy apki na androidzie

--- Moodly.Android/MainActivity.cs
+++ Moodly.Android/MainActivity.cs
@@ -6,7 +6,7 @@ using Avalonia.Android;
 namespace Moodly.Android;
 
 [Activity(
-    Label = "Moodly.Android",
+    Label = "Moodly",
     Theme = "@style/MyTheme.NoActionBar",
     Icon = "@drawable/icon",
     MainLauncher = true,
